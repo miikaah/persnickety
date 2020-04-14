7fdec833dc08a2f84c66bc873001a9d4fa92b35d
Miika Henttonen <miika.henttonen@wunderdog.fi>
2020-04-14 11:43:24 +0300

    Add basic schema generation functions

---
+++ src/index.mjs
@@ -0,0 +1,33 @@
+let schema;
+
+const Persnickety = (schemaSkeleton) => {
+  schema = schemaSkeleton
+
+  return {
+    getSchema: () => schema
+  }
+}
+
+export const Route = (path) => {
+  schema = {
+    ...schema,
+    paths: {
+      ...schema.paths,
+      ...path
+    }
+  }
+}
+
+export const Model = (model) => {
+  schema = {
+    ...schema,
+    components: {
+      schemas: {
+        ...(schema.components && schema.components.schemas),
+        ...model
+      }
+    }
+  }
+}
+
+export default Persnickety
